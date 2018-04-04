bfda7a83353be6f3ebc2e50ca35050829b11c745
sashaboulouds <33845251+sashaboulouds@users.noreply.github.com>
2018-04-04 19:24:37 +0200

    Add files via upload

---
+++ R/6_CAPM Exercise.R
@@ -0,0 +1,11 @@
+CAPM_Exercise <- read.csv("/Users/sashabouloudnine/Desktop/r/data/CAPM_Exercise_formated.csv", sep = ";")
+
+x<-CAPM_Exercise$`Returns.S.P`
+y<-CAPM_Exercise$`Return.AMZN`
+regression<-lm(y~x)
+plot(x,y)
+summary(regression)
+abline(regression,col="blue")
+Z<-(summary(regression)$coefficients[2,1]-1)/summary(regression)$coefficients[2,2]
+pnorm(Z)
+
